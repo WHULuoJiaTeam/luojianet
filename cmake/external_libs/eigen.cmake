95e361834a86b20439eb46b92757934bb1eaf002
MiZhangWhuer <zhangmi1989@163.com>
2022-03-31 14:10:22 +0000

    update cmake/external_libs/eigen.cmake.

--- cmake/external_libs/eigen.cmake
+++ cmake/external_libs/eigen.cmake
@@ -2,8 +2,8 @@ set(Eigen3_CXXFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 set(Eigen3_CFLAGS "-D_FORTIFY_SOURCE=2 -O2")
 
 
-set(REQ_URL "https://gitee.com/mizhangwhuer/eigen/repository/archive/3.3.9?format=zip")
-set(MD5 "d41d8cd98f00b204e9800998ecf8427e")
+set(REQ_URL "https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz")
+set(MD5 "609286804b0f79be622ccf7f9ff2b660")
 
 
 luojianet_ms_add_pkg(Eigen3

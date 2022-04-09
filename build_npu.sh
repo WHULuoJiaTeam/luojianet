bab2572b98aa56f9244ecae006ad1ea699aa7114
MiZhangWhuer <zhangmi1989@163.com>
2022-04-09 18:20:59 +0000

    update build_npu.sh.

--- build_npu.sh
+++ build_npu.sh
@@ -33,6 +33,26 @@ source ./scripts/build/process_options.sh
 source ./scripts/build/parse_device.sh
 source ./scripts/build/build_luojianet.sh
 
+#remove default patches for third party library
+PATCHES_FOLDER="${BUILD_PATH}/luojianet_ms/_ms_patch"
+if [ ! -d $LIB_FOLDER ]; then
+     rm -rf $PATCHES_FOLDER
+fi
+
+DEPS_FOLDER="${BUILD_PATH}/luojianet_ms/_deps"
+if [ ! -d $DEPS_FOLDER ]; then
+    rm -rf ${BUILD_PATH}/luojianet_ms/.mslib
+    rm -rf $DEPS_FOLDER
+    rm -rf $DEPS_FOLDER/*-src
+    rm -rf $DEPS_FOLDER/*-build
+    rm -rf $DEPS_FOLDER/*-subbuild/CMake*
+    rm -rf $DEPS_FOLDER/*-subbuild/Make*
+    rm -rf $DEPS_FOLDER/*-subbuild/cmake*
+    rm -rf $DEPS_FOLDER/*-subbuild/*-populate-prefix/src/*-stamp
+    rm -rf ${BUILD_PATH}/luojianet_ms/cmake*
+    rm -rf ${BUILD_PATH}/luojianet_ms/CMake*
+fi
+
 # check value of input is 'on' or 'off'
 # usage: check_on_off arg_value arg_name
 check_on_off()

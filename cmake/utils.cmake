bd7c69b1527063e9d6c41b6305592d1379511536
MiZhangWhuer <zhangmi1989@163.com>
2022-04-13 08:45:58 +0000

    update cmake/utils.cmake.

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -382,10 +382,10 @@ function(luojianet_ms_add_pkg pkg_name)
                     ${${pkg_name}_CMAKE_CFLAGS} ${${pkg_name}_CMAKE_CXXFLAGS} ${${pkg_name}_CMAKE_LDFLAGS}
                     -DCMAKE_INSTALL_PREFIX=${${pkg_name}_BASE_DIR} ${${pkg_name}_SOURCE_DIR}/${PKG_CMAKE_PATH}
                     WORKING_DIRECTORY ${${pkg_name}_SOURCE_DIR}/_build)
-                #__exec_cmd(COMMAND ${CMAKE_COMMAND} --build . --target install -- -j${THNUM}
-                 #       WORKING_DIRECTORY ${${pkg_name}_SOURCE_DIR}/_build)
-                __exec_cmd(COMMAND ${CMAKE_COMMAND} --build . --target install
+                __exec_cmd(COMMAND ${CMAKE_COMMAND} --build . --target install -- -j${THNUM}
                         WORKING_DIRECTORY ${${pkg_name}_SOURCE_DIR}/_build)
+                #__exec_cmd(COMMAND ${CMAKE_COMMAND} --build . --target install
+                 #       WORKING_DIRECTORY ${${pkg_name}_SOURCE_DIR}/_build)
             endif()
         else()
             if(${pkg_name}_CFLAGS)

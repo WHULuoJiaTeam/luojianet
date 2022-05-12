a17f54945f995a465994e10bb26ca28f46ced7d8
wenjiexuan <10599503+wenjiexuan@user.noreply.gitee.com>
2022-05-12 03:28:45 +0000

    init commit

---
+++ model_zoo/rs_object_detection/src/solver/__init__.py
@@ -0,0 +1,3 @@
+from .lr_schedule import dynamic_lr, a_cosine_learning_rate, linear_warmup_learning_rate
+
+__all__ = ["dynamic_lr", "a_cosine_learning_rate", "linear_warmup_learning_rate"]
---
+++ model_zoo/rs_object_detection/src/utils/__init__.py
@@ -0,0 +1,3 @@
+from .util import coco_eval_fasterrcnn, coco_eval_maskrcnn, get_seg_masks, get_seg_masks_inference, bbox2result_1image, results2json
+
+__all__ = ["coco_eval_fasterrcnn", "coco_eval_maskrcnn", "get_seg_masks", "get_seg_masks_inference", "bbox2result_1image", "results2json"]

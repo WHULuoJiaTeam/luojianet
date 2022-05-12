9be403f1648a637491033f1077f448ee3c1b5026
wenjiexuan <10599503+wenjiexuan@user.noreply.gitee.com>
2022-05-12 03:33:03 +0000

    init commit

---
+++ model_zoo/rs_object_detection/src/luojia_detection/detectors/__init__.py
@@ -0,0 +1,4 @@
+from .faster_rcnn import Faster_Rcnn_Resnet, FasterRcnn_Infer
+from .mask_rcnn import Mask_Rcnn_Resnet, MaskRcnn_Infer
+
+__all__ = ["Faster_Rcnn_Resnet", "FasterRcnn_Infer", "Mask_Rcnn_Resnet", "MaskRcnn_Infer"]

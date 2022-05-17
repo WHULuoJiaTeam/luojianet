70fb315df9349e7d848dcb6918acc02396688005
张震 <9832862+zhang-zhenss@user.noreply.gitee.com>
2022-05-17 11:03:17 +0000

    unet gid

---
+++ model_zoo/rs_semantic_segmentation/UNet_GID/losses.py
@@ -0,0 +1,129 @@
+import argparse
+
+import luojianet_ms.nn as nn
+import luojianet_ms.ops as P
+import luojianet_ms.common.dtype as mstype
+from luojianet_ms import Tensor, context
+import numpy as np
+
+# context.set_context(mode=context.PYNATIVE_MODE, device_target="CPU")
+
+class WeightedCrossEntropyLoss(nn.Module):
+    def __init__(self,
+                 cls_weight=None,
+                 ignore_label=-1,
+                 sparse=True):
+        super(WeightedCrossEntropyLoss, self).__init__()
+
+        self.ignore_label = ignore_label
+
+        self.cast = P.Cast()
+        self.ce = nn.SoftmaxCrossEntropyWithLogits(sparse=sparse, reduction='none')
+        self.not_equal = P.NotEqual()
+        self.equal = P.Equal()
+        self.zeros = P.Zeros()
+        self.ones = P.Ones()
+        self.mul = P.Mul()
+        self.sum = P.ReduceSum(False)
+        self.div = P.RealDiv()
+        self.transpose = P.Transpose()
+        self.reshape = P.Reshape()
+
+        if cls_weight is not None and cls_weight.dtype != mstype.float32:
+            self.cls_weight = self.cast(cls_weight, mstype.float32)
+        else:
+            self.cls_weight = cls_weight
+
+    def call(self, logits, labels):
+
+        # if logits.dim() == 4 and labels.dim() == 3:
+        num_cls = logits.shape[1]
+        labels_int = self.cast(labels, mstype.int32)
+        labels_int = self.reshape(labels_int, (-1,))
+        logits_ = self.transpose(logits, (0, 2, 3, 1))
+        logits_ = self.reshape(logits_, (-1, num_cls))
+
+        if self.cls_weight is None:
+            weights = self.not_equal(labels_int, self.ignore_label)
+            weights = self.cast(weights, mstype.float32)
+        else:
+            weights = self.zeros(labels_int.shape, mstype.float32)
+            for idx, ele in enumerate(self.cls_weight):
+                one_weight = self.equal(labels_int, idx)
+                one_weight = self.cast(one_weight, mstype.float32)
+                one_weight = one_weight * ele
+                weights = weights + one_weight
+
+        labels_int[labels_int == self.ignore_label] = 0
+
+        ce_loss = self.ce(logits_, labels_int)
+        ce_loss = self.mul(weights, ce_loss)
+        ce_loss = self.div(self.sum(ce_loss), self.sum(weights))
+        return ce_loss
+
+def build_criterion(args):
+
+    print("=> Trying bulid {:}loss".format(args.criterion))
+
+    if args.criterion == 'ce':
+        return WeightedCrossEntropyLoss(cls_weight=args.weight, ignore_label=args.ignore_label, sparse=True)
+    else:
+        raise ValueError('unknown criterion : {:}'.format(args.criterion))
+class CrossEntropyLoss(nn.Module):
+    def __init__(self, sparse=True):
+        super(CrossEntropyLoss, self).__init__()
+        self.cast = P.Cast()
+        self.ce = nn.SoftmaxCrossEntropyWithLogits(sparse=sparse, reduction='mean')
+        self.transpose = P.Transpose()
+        self.reshape = P.Reshape()
+
+    def call(self, logits, labels):
+        num_cls = logits.shape[1]
+        labels_int = self.cast(labels, mstype.int32)
+        labels_int = self.reshape(labels_int, (-1,))
+        logits_ = self.transpose(logits, (0, 2, 3, 1))
+        logits_ = self.reshape(logits_, (-1, num_cls))
+
+        ce_loss = self.ce(logits_, labels_int)
+
+        return ce_loss
+
+class MCEloss(nn.loss.loss._Loss):
+    def __init__(self, reduction='mean'):
+        super().__init__(reduction)
+        self.creti = nn.SoftmaxCrossEntropyWithLogits(sparse=True)
+
+    def call(self, data, label):
+        batch, channel, height, width = P.Shape()(data)
+        data = P.Transpose()(data, (0,2,3,1,))
+        data = P.Reshape()(data, (batch * height * width, channel))
+        batch, height, width = P.Shape()(label)
+        label = P.Reshape()(label, (batch * height * width,))
+        x = self.creti(data,label)
+        return self.get_loss(x)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="luojianet_ms Loss")
+    parser.add_argument('--ignore_label', type=int, default=-1, help='weight standardization')
+    Args = parser.parse_args()
+
+    weights = np.array([1, 2, 3, 4])
+
+    Args.weight = Tensor(weights)
+    Args.criterion = 'ce'
+
+    wce_loss_luojia = build_criterion(Args)
+
+    uniformreal = P.UniformReal(seed=2)
+
+    a = uniformreal((3, 4, 769, 769))
+    b = np.random.randint(0, 5, size=(3, 769, 769), dtype=np.int32) - 1
+    b = Tensor(b)
+
+    #luojianet
+    a_luojia = Tensor(a)
+    b_luojia = Tensor(b)
+
+    print(a_luojia)
+    print(b_luojia)
+
+    print(wce_loss_luojia(a_luojia, b_luojia))
---
+++ model_zoo/rs_semantic_segmentation/UNet_GID/train_unet_new.py
@@ -0,0 +1,119 @@
+import os
+from tqdm import tqdm
+from dataloder_gid_aug import Dataset_RealUD
+from unet import unet
+# from model1 import DeepLabV3Plus_RFB
+import luojianet_ms
+from luojianet_ms import dataset
+import luojianet_ms.nn as ljnn
+from losses import MCEloss, WeightedCrossEntropyLoss
+import luojianet_ms.common.dtype as mstype
+from metrics import SegmentationMetric
+import luojianet_ms.ops.operations as P
+
+def run_Gid(ClassN):
+    ##################### define parameters #############################################################
+    epochs = 120
+    initial_learning_rate = 0.0001
+    eta_min = 1e-5
+    weight_decay = 1e-5
+    batch_size_train = 20
+    batch_size_test = 20
+    mioubest = 0
+
+
+    dataset_train = r'/media/vhr/0D7A09740D7A0974/zz/GID/LCC5C/LCC5C_b512_woOverlap.json'
+    dataset_test = r'/media/vhr/0D7A09740D7A0974/zz/GID/LCC5C/LCC5C_b512_woOverlap_test.json'
+    ##################### load data ###################################################################
+    train_dataset_generator = Dataset_RealUD(json_path=dataset_train, train=True)
+    train_loader = dataset.GeneratorDataset(train_dataset_generator, ["data", "label"], shuffle=True)
+    train_loader = train_loader.batch(batch_size_train)
+
+    test_dataset_generator = Dataset_RealUD(json_path=dataset_test)
+    test_loader = dataset.GeneratorDataset(test_dataset_generator, ["data", "label"], shuffle=False)
+    test_loader = test_loader.batch(batch_size_test)
+
+    ###########################################   define model    #########################################
+    model = unet(output_class=ClassN)
+    # luojianet_ms.load_param_into_net(model, luojianet_ms.load_checkpoint(
+    #     '/media/vhr/0D7A09740D7A0974/zz/Luojianet_ckpt/unet/unet_newest.ckpt'))
+    step_size = train_loader.get_dataset_size()
+    criterion = WeightedCrossEntropyLoss(ignore_label=ClassN)
+    scheduler = ljnn.cosine_decay_lr(min_lr=eta_min, max_lr=initial_learning_rate, total_step=epochs*step_size, step_per_epoch=step_size, decay_epoch=120)
+    optimizer = ljnn.AdamWeightDecay(model.trainable_params(), learning_rate=scheduler, weight_decay=weight_decay)
+    # optimizer = ljnn.AdamWeightDecay(model.trainable_params(), learning_rate=1e-5, weight_decay=weight_decay)
+
+    net_with_criterion = luojianet_ms.nn.WithLossCell(model, criterion)
+    train_net = luojianet_ms.nn.TrainOneStepCell(net_with_criterion, optimizer)
+
+    metric = SegmentationMetric(ClassN)
+    for epoch in range(epochs):
+        model.set_train(True)
+        model.set_grad(True)
+        loss_val_sum = 0
+        printcount = 0
+        for train_x, train_y in tqdm(train_loader):
+            printcount += 1
+            train_x = train_x[:, 0, :, :, :]
+            train_x = luojianet_ms.ops.Cast()(train_x, mstype.float32)
+            train_y = luojianet_ms.ops.Cast()(train_y, mstype.int32)
+            train_net(train_x, train_y)
+            loss_val = net_with_criterion(train_x, train_y)
+            loss_val_sum = loss_val_sum + loss_val
+            if printcount % 100 == 0:
+                print(loss_val_sum / printcount)
+
+        # ///////////////
+
+        metric.reset()
+        model.set_train(False)
+        model.set_grad(False)
+        for input, target in tqdm(test_loader):
+            input = input[:, 0, :, :, :]
+            input = luojianet_ms.ops.Cast()(input, mstype.float32)
+            target = luojianet_ms.ops.Cast()(target, mstype.int32)
+            output = model(input)
+
+            output_v = P.ArgMaxWithValue(axis=1, keep_dims=False)(output)[0].asnumpy()
+            target = target.asnumpy()
+            # target[target == 6] = 0
+            metric.addBatch(output_v, target)
+
+        validate_result = metric.IntersectionOverUnion() * 100
+        print([round(i, 3) for i in validate_result])
+        MIOU_target = sum(validate_result[1:])/(len(validate_result)-1)
+        MIOU = sum(validate_result)/len(validate_result)
+        print('Epoch:', epoch,'MIOU-target:', MIOU_target, 'MIOU:', MIOU)
+
+        luojianet_ms.save_checkpoint(model, '/media/vhr/0D7A09740D7A0974/zz/Luojianet_ckpt/unet/unet_'+str(epoch)+'.ckpt')
+        # if MIOU_target > mioubest:
+        #     mioubest=MIOU_target
+        #     luojianet_ms.save_checkpoint(model, '/media/vhr/0D7A09740D7A0974/zz/Luojianet_ckpt/unet/unet_best.ckpt')
+
+
+
+
+if __name__ == '__main__':
+    import luojianet_ms.context as context
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    context.set_context(mode=context.GRAPH_MODE, device_target="GPU")
+    run_Gid(6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

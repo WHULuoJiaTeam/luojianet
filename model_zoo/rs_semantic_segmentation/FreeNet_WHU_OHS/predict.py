79f62ebc3ff315a434615dc9d1755df5f20be4f8
Lilin Tu <tulilin0312@163.com>
2022-05-20 12:18:52 +0000

    update model_zoo/rs_semantic_segmentation/FreeNet_WHU_OHS/predict.py.

--- model_zoo/rs_semantic_segmentation/FreeNet_WHU_OHS/predict.py
+++ model_zoo/rs_semantic_segmentation/FreeNet_WHU_OHS/predict.py
@@ -1,3 +1,5 @@
+# FreeNet网络预测
+
 import os
 import argparse
 import numpy as np
@@ -22,6 +24,7 @@ IMG_EXTENSIONS = [
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
+# 输出预测结果影像的函数
 def writeTiff(im_data, im_width, im_height, im_bands, path):
     if 'int8' in im_data.dtype.name:
         datatype = gdal.GDT_Byte
@@ -44,6 +47,7 @@ def writeTiff(im_data, im_width, im_height, im_bands, path):
         dataset.GetRasterBand(i+1).WriteArray(im_data[i])
     del dataset
 
+# 定义预测过程
 class MyWithEvalCell(nn.Module):
     def __init__(self, network):
         super(MyWithEvalCell, self).__init__(auto_prefix=False)
@@ -79,7 +83,7 @@ def main():
             reduction_ratio=1.0,
         )
 
-    # Model
+    # 加载模型
     print('Build model ...')
     net = FreeNet(config=config_net)
     model_path = args_opt.checkpoint_path
@@ -102,12 +106,14 @@ def main():
 
     softmax = ops.Softmax(axis=0)
     argmax = ops.Argmax(axis=0)
-
+    
+    # 读取待预测影像
     image_dataset = gdal.Open(args_opt.input_file, gdal.GA_ReadOnly)
     image = image_dataset.ReadAsArray().astype(np.float32)
     width = image.shape[2]
     height = image.shape[1]
-
+    
+    # 若需要对影像进行归一化，则逐波段进行标准差归一化
     if(config['normalize']):
         eps = 1e-8
         bands = image.shape[0]
@@ -122,7 +128,8 @@ def main():
 
     image = np.expand_dims(image, axis=0)
     image = Tensor(image)
-
+    
+    # 网络预测
     output = model.predict(image)
     output = output.squeeze()
     output = softmax(output)
@@ -131,7 +138,8 @@ def main():
     output = output.asnumpy() + 1
 
     output = output.astype(np.uint8)
-
+    
+    # 保存结果
     fname = os.path.basename(args_opt.input_file)
     writeTiff(output, width, height, 1, os.path.join(save_path, fname))
 

a4eba8066e492f7debde37e610469ebc5af84384
Lilin Tu <tulilin0312@163.com>
2022-05-22 13:57:35 +0000

    update model_zoo/rs_semantic_segmentation/HRNet3D_WHU_OHS/predict.py.

--- model_zoo/rs_semantic_segmentation/HRNet3D_WHU_OHS/predict.py
+++ model_zoo/rs_semantic_segmentation/HRNet3D_WHU_OHS/predict.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import math
 import numpy as np
 from luojianet_ms import Model
 from luojianet_ms import Tensor
@@ -45,6 +46,46 @@ def writeTiff(im_data, im_width, im_height, im_bands, path):
         dataset.GetRasterBand(i+1).WriteArray(im_data[i])
     del dataset
 
+# 滑动窗口预测整景影像
+def predict_whole_image(model, image, grid, stride):
+    overlap = grid - stride
+    invalid_num = int(overlap / 2)
+
+    n, b, r, c = image.shape
+    rows = -((grid - r) // (stride + 1e-10)) * stride + grid
+    cols = -((grid - c) // (stride + 1e-10)) * stride + grid
+    rows = math.ceil(rows)
+    cols = math.ceil(cols)
+    image_ = np.pad(image, ((0, 0), (0, 0), (0, rows - r), (0, cols - c)), 'symmetric')
+
+    output = np.zeros((rows, cols), dtype=np.uint8)
+
+    expand_dims = ops.ExpandDims()
+    softmax = ops.Softmax(axis=0)
+    argmax = ops.Argmax(axis=0)
+
+    for i in range(0, rows, stride):
+        print('Current row:', i)
+        for j in range(0, cols, stride):
+            patch = image_[0:, 0:, i:i + grid, j:j + grid]
+            patch = Tensor(patch)
+            patch = expand_dims(patch, 1)
+
+            pred = model.predict(patch)
+
+            pred = pred.squeeze()
+            pred = softmax(pred)
+            pred = argmax(pred)
+            pred = pred.asnumpy() + 1
+            pred = pred.astype(np.uint8)
+
+            output[i + invalid_num:i + grid - invalid_num, j + invalid_num:j + grid - invalid_num] = \
+                pred[invalid_num:grid - invalid_num, invalid_num:grid - invalid_num]
+
+    output = output[0:r, 0:c]
+
+    return output
+
 def main():
     parser = argparse.ArgumentParser()
 
@@ -57,7 +98,7 @@ def main():
 
     context.set_context(mode=context.GRAPH_MODE, device_target=args_opt.device_target)
 
-    # 加载网络模型
+    # 加载模型
     print('Build model ...')
     net = HigherHRNet_Binary(num_classes=config['classnum'], hr_cfg='w18_3d2d_at')
     model_path = args_opt.checkpoint_path
@@ -73,42 +114,30 @@ def main():
     save_path = args_opt.output_folder
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-
-    softmax = ops.Softmax(axis=0)
-    argmax = ops.Argmax(axis=0)
-
+    
     # 读取待预测影像
     image_dataset = gdal.Open(args_opt.input_file, gdal.GA_ReadOnly)
-    image = image_dataset.ReadAsArray()
+    image = image_dataset.ReadAsArray().astype(np.float32)
     width = image.shape[2]
     height = image.shape[1]
+    
+    # 逐波段进行标准差归一化
+    eps = 1e-8
+    bands = image.shape[0]
+    image_new = np.zeros_like(image)
+    for i in range(bands):
+        image_i = image[i, :, :]
+        mean = np.mean(image_i)
+        std = np.std(image_i)
+        image_new[i, :, :] = (image_i - mean) / (std + eps)
 
-    # 若需要对影像进行归一化，则逐波段进行标准差归一化
-    if(config['normalize']):
-        eps = 1e-8
-        bands = image.shape[0]
-        image_new = np.zeros_like(image)
-        for i in range(bands):
-            image_i = image[i, :, :]
-            mean = np.mean(image_i)
-            std = np.std(image_i)
-            image_new[i, :, :] = (image_i - mean) / (std + eps)
-
-        image = image_new
+    image = image_new
 
     image = np.expand_dims(image, axis=0)
-    image = np.expand_dims(image, axis=0)
-    image = Tensor(image)
     
     # 网络预测
-    output = model.predict(image)
-    output = output.squeeze()
-    output = softmax(output)
-    output = argmax(output)
-
-    output = output.asnumpy() + 1
-
-    output = output.astype(np.uint8)
+    print('Predicting.')
+    output = predict_whole_image(model, image, grid=256, stride=192)
     
     # 保存结果
     fname = os.path.basename(args_opt.input_file)
